e6ab3b614c04c35d3fe500e7b575adc778fe96af
lesliemarlo <lesliemarlo09@gmail.com>
2025-05-12 10:31:46 -0500

    cors_cpanel

--- src/main/java/com/informaperu/web/registropagos/security/CorsConfig.java
+++ src/main/java/com/informaperu/web/registropagos/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://registro-pago-informaperu-front.vercel.app/login") // Origen permitido
+                    .allowedOrigins("https://pagos.informaperu.com") // Origen permitido
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
